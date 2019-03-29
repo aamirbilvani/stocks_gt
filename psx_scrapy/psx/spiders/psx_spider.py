0982773b3fff58cc0a8041634517a36a6560da65
Aamir Bilvani <aamir.bilvani@gmail.com>
2019-03-29 11:43:12 +0500

    reworked the crawler to record new datapoints in new rows instead of new columns to grow th sheet vertically instead of horizontally over time

--- psx_scrapy/psx/spiders/psx_spider.py
+++ psx_scrapy/psx/spiders/psx_spider.py
@@ -26,46 +26,46 @@ class PSXSpider(scrapy.Spider):
             for line in urls_file:
                 urls.append(line.strip())
 
-        col_num = 0
+        row_num = 0
         timestamp = '{:02d}:{:02d}'.format(current_date.hour, current_date.minute)
-        if str(ws.cell(1,ws.max_column).value).strip() == timestamp:
-            col_num = ws.max_column
-        elif str(ws.cell(1,ws.max_column).value).strip() == '':
-            col_num = ws.max_column
-            ws.cell(1,col_num).value = timestamp
+        if str(ws.cell(ws.max_row, 1).value).strip() == timestamp:
+            row_num = ws.max_row
+        elif str(ws.cell(ws.max_row, 1).value).strip() == '':
+            row_num = ws.max_row
+            ws.cell(row_num, 1).value = timestamp
         else:
-            col_num = ws.max_column + 1
-            ws.cell(1,col_num).value = timestamp
+            row_num = ws.max_row + 1
+            ws.cell(row_num, 1).value = timestamp
 
         for url in urls:
             request = scrapy.Request(url=url, callback=self.parse)
             request.meta['workbook'] = wb
-            request.meta['col_num'] = col_num
+            request.meta['row_num'] = row_num
             request.meta['path'] = path
             yield request
 
     def parse(self, response):
         wb = response.meta['workbook']
-        col_num = response.meta['col_num']
+        row_num = response.meta['row_num']
         path = response.meta['path']
         ws = wb.active
 
         symbol = response.css('div.pageHeader__title::text').extract_first()
         stock_value = response.css('div.quote__close::text').extract_first()[3:]
 
-        row_num = 0
-        for i in range(1, ws.max_row + 1):
-            row_symbol = str(ws.cell(i,1).value).strip()
-            if row_symbol == symbol:
-                row_num = i
-                ws.cell(i,col_num).value = stock_value
+        col_num = 0
+        for i in range(1, ws.max_column + 1):
+            col_symbol = str(ws.cell(1, i).value).strip()
+            if col_symbol == symbol:
+                col_num = i
+                ws.cell(row_num, i).value = float(stock_value)
                 wb.save(path)
                 break
         
-        if row_num == 0:
-            row_num = ws.max_row + 1
-            ws.cell(row_num, 1).value = symbol
-            ws.cell(row_num, col_num).value = stock_value
+        if col_num == 0:
+            col_num = ws.max_column + 1
+            ws.cell(1, col_num).value = symbol
+            ws.cell(row_num, col_num).value = float(stock_value)
             wb.save(path)
 
                 
